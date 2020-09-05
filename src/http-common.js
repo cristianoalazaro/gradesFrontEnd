4ec3f259c9dda09e12b7fcbec18345991de1fbae
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2020-09-05 18:19:38 -0300

    Alterando confs

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://grades-back3.herokuapp.com/',
+  baseURL:
+    process.env.REACT_APP_PORT_API === ''
+      ? process.env.REACT_APP_HOST_API
+      : `${process.env.REACT_APP_HOST_API}:${process.env.REACT_APP_PORT_API}`,
   headers: {
     'Content-type': 'application/json',
   },
