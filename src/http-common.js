9b76478bb43b111fb7ac7f6e61a62e5faa285ea7
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2020-09-06 06:46:12 -0300

    altera common

--- src/http-common.js
+++ src/http-common.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL:
-    process.env.REACT_APP_PORT_API === ''
+  //baseURL:
+  /*process.env.REACT_APP_PORT_API === ''
       ? process.env.REACT_APP_HOST_API
-      : `${process.env.REACT_APP_HOST_API}:${process.env.REACT_APP_PORT_API}`,
+      : `${process.env.REACT_APP_HOST_API}:${process.env.REACT_APP_PORT_API}`,*/
+  baseURL: 'https://grades-back3.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
